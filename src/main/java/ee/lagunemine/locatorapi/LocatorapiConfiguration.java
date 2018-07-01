3ce3d5453b2fe2aec3674be64bcaaa1e8a1f2f11
Artem Antcev <artemy@evercodelab.com>
2018-07-01 14:50:25 +0300

    Added the robust exception handling for controller

--- src/main/java/ee/lagunemine/locatorapi/LocatorapiConfiguration.java
+++ src/main/java/ee/lagunemine/locatorapi/LocatorapiConfiguration.java
@@ -1,6 +1,8 @@
 package ee.lagunemine.locatorapi;
 
 import org.modelmapper.ModelMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -8,8 +10,15 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ComponentScan
 public class LocatorapiConfiguration {
+    private static final String LOGGER_NAME = "application";
+
     @Bean
     public ModelMapper mapper() {
         return new ModelMapper();
+    }
+
+    @Bean
+    public Logger logger(){
+        return LoggerFactory.getLogger(LOGGER_NAME);
     }
 }
--- src/main/java/ee/lagunemine/locatorapi/controller/StationController.java
+++ src/main/java/ee/lagunemine/locatorapi/controller/StationController.java
@@ -3,20 +3,54 @@ package ee.lagunemine.locatorapi.controller;
 import ee.lagunemine.locatorapi.dto.StationBaseMessageDTO;
 import ee.lagunemine.locatorapi.dto.StationMobilePositionDTO;
 import ee.lagunemine.locatorapi.exception.MissingStationMobileException;
+import ee.lagunemine.locatorapi.model.StationMobile;
 import ee.lagunemine.locatorapi.service.StationService;
 import org.modelmapper.ModelMapper;
+import org.slf4j.Logger;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.core.annotation.AnnotatedElementUtils;
+
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import java.util.*;
 
 @RestController
 @RequestMapping("/stations")
 class StationController {
     private StationService stationService;
     private ModelMapper mapper;
+    private Logger logger;
 
-    public StationController(StationService stationService, ModelMapper mapper) {
+    private final static String ERROR_INTERNAL = "An internal error has occurred";
+    private final static String ERROR_LOG_PREFIX = "An exception in controller has been occurred";
+
+    public StationController(StationService stationService, ModelMapper mapper, Logger logger) {
         this.stationService = stationService;
         this.mapper = mapper;
+        this.logger = logger;
+    }
+
+    @ExceptionHandler(value = {Exception.class, RuntimeException.class})
+    public Map<String, String> handleDefaultErrors(HttpServletRequest request, Exception e) {
+        HttpStatus httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
+        ResponseStatus annotation = AnnotatedElementUtils.findMergedAnnotation(e.getClass(), ResponseStatus.class);
+
+        if (annotation != null) {
+            httpStatus = annotation.value();
+        }
+
+        Map<String, String> error = new HashMap<>();
+
+        error.put("code", httpStatus.toString());
+        error.put("error", httpStatus != HttpStatus.INTERNAL_SERVER_ERROR ? e.getMessage() : ERROR_INTERNAL);
+        error.put("path", request.getRequestURL().toString());
+        error.put("time", new Date().toString());
+
+        logger.error(ERROR_LOG_PREFIX, e);
+
+        return error;
     }
 
     @PostMapping("/update")
@@ -26,14 +60,13 @@ class StationController {
 
     @GetMapping("/mobile/find")
     @ResponseBody
-    public StationMobilePositionDTO getMobileStationPosition(int stationId) {
+    public StationMobilePositionDTO getMobileStationPosition(
+            @RequestParam(value = "stationId") @NotNull Integer id) throws MissingStationMobileException {
         StationMobilePositionDTO response = new StationMobilePositionDTO();
 
-        try {
-            mapper.map(stationService.getMobileStation(stationId), response);
-        } catch (MissingStationMobileException e) {
+        StationMobile lol = stationService.getMobileStation(id);
 
-        }
+        mapper.map(stationService.getMobileStation(id), response);
 
         return response;
     }
